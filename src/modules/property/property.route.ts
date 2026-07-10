4359c0804f05fb59d3b37f52d8805842980775a2
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-07-10 17:58:13 +0600

    properties role define

--- src/modules/property/property.route.ts
+++ src/modules/property/property.route.ts
@@ -3,19 +3,27 @@ import { PropertyController } from './property.controller';
 import { auth } from '../../middleware/auth'; 
 import { Role } from '../../../generated/prisma/enums';
 
+
+
+
 const router = express.Router();
 
+
 // Publicly Properties
-router.get('/properties', auth( Role.ADMIN, Role.LANDLORD, Role.TENANT ), PropertyController.getAllProperties);
+router.get('/properties', PropertyController.getAllProperties);
 
 
 // Only Authenticated Landlord
 router.post('/landlord/properties', auth( Role.LANDLORD ), PropertyController.createProperty);
+router.get('/landlord/properties', auth( Role.LANDLORD ), PropertyController.getAllProperties);
 router.patch('/landlord/properties/isAvailable/:propertyId', auth( Role.LANDLORD ), PropertyController.toggleAvailability);
 router.delete('/landlord/properties/:propertyId', auth( Role.LANDLORD ), PropertyController.deleteProperty);
 router.put('/landlord/properties/:propertyId', auth( Role.LANDLORD ), PropertyController.updateProperty);
 
 
+
+// Admin Properties 
+router.get('/admin/properties', auth( Role.ADMIN ), PropertyController.getAllProperties);
 // Change Property Status Only for Admin
 router.patch(
     '/admin/properties/change-status/:propertyId', 
