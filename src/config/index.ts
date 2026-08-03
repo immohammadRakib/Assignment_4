348ad72eff23534b2ef535d4fd5ff7a3883e62fa
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-08-03 18:36:17 +0600

    url update

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ export default {
     port: process.env.PORT || 5000,
     database_url: process.env.DATABASE_URL || "", 
     app_url: process.env.APP_URL,
-    client_url: process.env.CLIENT_URL || "http://localhost:3000",
+    client_url: process.env.CLIENT_URL || "https://assignment-5-git-main-rakiblp6gmailcoms-projects.vercel.app",
     bcryptSaltRounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 10,
     jwt_access_secret: process.env.JWT_ACCESS_SECRET || "",
     jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || "",
